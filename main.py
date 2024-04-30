43a5e42ba9daf7509cbd853d279d27b13021e8b4
kasshii28 <gp22a043@oecu.jp>
2024-04-30 18:45:15 +0900

    データベース、APIの追加

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from database import create_database
+from routes.API import router as API_route
+
+create_database()
+app = Flask(__name__, static_url_path="", static_folder="static")
+
+@app.route("/")
+def index():
+    return app.send_static_file("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port=8000)
